58f2558547378091e8820bb5a1d7ec7a7c563103
Dario Antonio Rivera <dario.rivera@enertolima.com>
2016-09-02 10:30:45 -0500

    Add support to Zend_Validate

--- Framework/autoload.php
+++ Framework/autoload.php
@@ -25,3 +25,7 @@ function FrameworkLoader($name)
 }
 
 spl_autoload_register("FrameworkLoader");
+
+# load vendor classes
+if (file_exists(dirname(__FILE__) . '/../vendor/autoload.php'))
+	require_once(dirname(__FILE__) . '/../vendor/autoload.php');
---
+++ module/App/source/controller/App_Controller_Form.php
@@ -0,0 +1,57 @@
+<?php
+
+class App_Controller_Form extends AbstractionController
+{
+	public function validator()
+	{
+		# data to view
+		$data = array();
+
+		if ($this->isPost())
+		{
+			$data["success"] = true;
+
+			$rules = array(
+				"fname" => array(
+					"required" => true,
+					/*"minlength" => 3,
+					"maxlength" => 10,
+					"alnumWhiteSpace" => true,*/
+					"label" => "First name"
+				),
+				"lname" => array(
+					"required" => true,
+					/*"minlength" => 3,
+					"maxlength" => 10,
+					"alnumWhiteSpace" => true,*/
+					"label" => "Last name"
+				),
+				"age" => array(
+					"required" => true,
+					"digits" => true,
+					"label" => "Age"
+				)
+			);
+
+			try {
+				$validator = new QuickValidator($rules);
+				$validator->validateWith($_POST);
+
+				if (!$validator->isValid())
+				{
+					$data["success"] = false;
+					$data["messages"] = $validator->getMessages();
+				}
+
+			}
+			catch (Exception $e)
+			{
+				$data["success"] = false;
+				$data["message"] = $e->getMessage();
+				return $data;
+			}
+		}
+
+		return $data;
+	}
+}
