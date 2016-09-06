f3a98e762a2c63a540271e535fd330213be1a80a
Darío Rivera <fermius.us@gmail.com>
2016-09-06 15:37:46 -0500

    Delete unused libraries

--- public/index.php
+++ public/index.php
@@ -8,30 +8,8 @@ date_default_timezone_set("America/Bogota");
 // Memory limit
 ini_set("memory_limit","256M");
 
-// Directories to load
-$directories = array("library");
-
-// General library loader
-function LibraryLoader($name)
-{
-	global $directories;
-
-	foreach ($directories as $dir)
-	{
-		$class =  dirname(dirname(__FILE__)) . "/$dir/". str_replace('\\', '/', $name) . ".php";
-
-		if (file_exists($class))
-		{
-			include $class;
-			break;
-		}
-	}
-}
-
-// Load libraries
-spl_autoload_register("LibraryLoader");
-
 // Run application
 require_once("Framework/autoload.php");
+
 $mvc = new Drone_Mvc_Application(include "config/application.config.php");
 $mvc->run();
