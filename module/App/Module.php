9aaf5cf9d36067d2044b7082a898a02e6e3e141e
Darío Rivera <fermius.us@gmail.com>
2017-03-23 09:36:01 -0500

    Delete skeleton app

--- module/App/Module.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class App_Module extends Drone_Mvc_AbstractionModule
-{
-	public function init(Drone_Mvc_AbstractionController $c)
-	{
-		// init procedures
-	}
-}
--- module/App/source/controller/App_Controller_Dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class App_Controller_Dashboard extends Drone_Mvc_AbstractionController
-{
-	public function index()
-	{
-		return array();
-	}
-}
