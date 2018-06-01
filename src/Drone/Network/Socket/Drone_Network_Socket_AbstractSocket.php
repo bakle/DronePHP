e405275d09c5410d14593572f3690b25207024bd
Darío Rivera <fermius.us@gmail.com>
2018-06-01 09:11:32 -0500

    Add the first Socket implementation

---
+++ src/Drone/Network/Socket/Drone_Network_Socket_AbstractSocket.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * DronePHP (http://www.dronephp.com)
+ *
+ * @link      http://github.com/fermius/DronePHP
+ * @copyright Copyright (c) 2016-2018 Pleets. (http://www.pleets.org)
+ * @license   http://www.dronephp.com/license
+ */
+
+class Drone_Network_Socket_AbstractSocket
+{
+    /**
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * @var integer
+     */
+   protected $port;
+
+    /**
+     * Socket resource
+     *
+     * @var resource|boolean
+     */
+    protected $socket;
+
+    /**
+     * Error collector
+     *
+     * @var Drone_Error_ErrorCollector
+     */
+    protected $errorProvider;
+
+    /**
+     * Returns the host attribute
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Returns the port attribute
+     *
+     * @return integer
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Returns the socket attribute
+     *
+     * @return resource|boolean
+     */
+    public function getSocket()
+    {
+        return $this->socket;
+    }
+
+    /**
+     * Sets host attribute
+     *
+     * @param string $host
+     *
+     * @return null
+     */
+    public function setHost($host)
+    {
+        return $this->host = $host;
+    }
+
+    /**
+     * Sets port attribute
+     *
+     * @param integer $port
+     *
+     * @return null
+     */
+    public function setPort($port)
+    {
+        return $this->port = $port;
+    }
+
+    /**
+     * Driver Constructor (here socket will be created!)
+     *
+     * All modifiable attributes (i.e. with setter method) can be passed as key
+     *
+     * @throws RuntimeException
+     *
+     * @param array $options
+     */
+   public function __construct($options)
+   {
+        foreach ($options as $option => $value)
+        {
+            if (property_exists(__CLASS__, strtolower($option)) && method_exists($this, 'set'.$option))
+                $this->{'set'.$option}($value);
+        }
+
+       if (!($this->socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP)))
+       {
+            $errno = socket_last_error();
+            $this->errorProvider->error($errno, socket_strerror($errno));
+
+            throw new RuntimeException("Could not create the socket");
+       }
+
+        $this->errorProvider->errorProvider = new Drone_Error_ErrorCollector();
+   }
+
+    /**
+     * Binds the socket
+     *
+     * @return bool
+     */
+    public function bind()
+    {
+        if (!($bind = @socket_bind($this->socket, $this->host, $this->port)))
+        {
+            $errno = socket_last_error();
+            $this->errorProvider->error($errno, socket_strerror($errno));
+            return false;
+        }
+
+        return $bind;
+    }
+
+    /**
+     * Closes the socket
+     *
+     * @return bool
+     */
+   public function close()
+   {
+       return socket_close($this->socket);
+   }
+}
---
+++ src/Drone/Network/Socket/Drone_Network_Socket_Server.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * DronePHP (http://www.dronephp.com)
+ *
+ * @link      http://github.com/fermius/DronePHP
+ * @copyright Copyright (c) 2016-2018 Pleets. (http://www.pleets.org)
+ * @license   http://www.dronephp.com/license
+ */
+
+class Drone_Network_Socket_Server
+{
+    /**
+     * Reads a message from client
+     *
+     * @param resource
+     *
+     * @throws RuntimeException
+     *
+     * @return string
+     */
+   public function read($socket)
+   {
+       if (($message = @socket_read($socket, 1024)) === false)
+       {
+            $errno = socket_last_error();
+            $this->error($errno, socket_strerror($errno));
+
+            throw new RuntimeException("Could not read message from client socket");
+       }
+
+       return $message;
+   }
+
+    /**
+     * Sends a message to client socket
+     *
+     * @param resource $socket
+     * @param string $message
+     *
+     * @throws RuntimeException
+     *
+     * @return integer
+     */
+   public function send($socket, $message)
+   {
+       if (($bytes = @socket_write($socket, $message, strlen($message))) === false)
+       {
+            $errno = socket_last_error();
+            $this->error($errno, socket_strerror($errno));
+
+            throw new RuntimeException("Could not send message to the client socket");
+       }
+
+       return $bytes;
+   }
+
+    /**
+     * Sets socket to listening
+     *
+     * @param array $handlers
+     *
+     * @throws RuntimeException
+     *
+     * @return null
+     */
+   public function listen(Array $eventHandlers = array())
+   {
+       $event = $eventHandlers;
+       $clousure = function(){};
+
+       if (!array_key_exists('success', $event))
+           $event["success"] = $clousure;
+
+       if (!array_key_exists('error', $event))
+           $event["error"] = $clousure;
+
+       $listener = false;
+
+       if (!($listener = @socket_listen($this->socket, 30)))
+       {
+            $errno = socket_last_error();
+            $this->error($errno, socket_strerror($errno));
+
+            throw new RuntimeException("Could not set socket to listen");
+       }
+       else {
+
+           echo "\n";
+           echo "Server Started : " . date('Y-m-d H:i:s') . "\n";
+           echo "Master socket  : " . $this->socket . "\n";
+           echo "Listening on   : " . $this->host . " port " . $this->port . "\n\n";
+
+           $socket = $this->socket;
+
+           if (!($spawn = @socket_accept($this->socket)))
+           {
+               $errno = socket_last_error();
+               $this->error($errno, socket_strerror($errno));
+
+               throw new RuntimeException("Could not accept incoming connection");
+           }
+
+           $input = $this->read($spawn);
+
+           $input = trim($input);
+           call_user_func($event["success"], $input, $this, $spawn);
+
+           socket_close($spawn);
+       }
+
+       if (!$listener)
+           call_user_func($event["error"], $this->getLastError());
+
+       return $listener;
+   }
+}
